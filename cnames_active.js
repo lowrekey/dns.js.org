eff5d81d81995673cfbc8b3f8fa8128de1fb4f67
Joshua Moore <lowrekey@gmail.com>
2015-08-20 16:33:03 -0700

    Update cnames_active.js
    lowrekey.github.io/fourd.js

--- cnames_active.js
+++ cnames_active.js
@@ -284,4 +284,5 @@ var cnames_active = {
     ,"youtube-box": "lucasmonteverde.github.io/youtube-box"
     ,"zodiac": "indus.github.io/Zodiac"
     ,"zombie": "assaf.github.io/zombie"
+    ,"fourd" : "lowrekey.github.io/fourd.js"
 }
